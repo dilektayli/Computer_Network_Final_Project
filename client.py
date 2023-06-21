6d7eda3d8ef289df770d5393231b3c200a4cad7d
kdxxx <83818961+kdxxx@users.noreply.github.com>
2023-06-22 01:42:45 +0300

    renamed client2.py as client.py
    removed client.py and rename client2.py

--- client.py
+++ client.py
@@ -1,13 +1,17 @@
 from tkinter import Tk, Frame, Scrollbar, Label, END, Entry, Text, VERTICAL, Button, \
-    messagebox  # Tkinter Python Module for GUI
+    messagebox,filedialog
+from PIL import ImageTk, Image
+import requests
 import socket  # Sockets for network connection
-import threading  # for multiple proccess
+import threading  # for multiple process
+
 
 
 class GUI:
     client_socket = None
     last_received_message = None
 
+    # Create widgets to initialize GUI
     def __init__(self, master):
         self.root = master
         self.chat_transcript_area = None
@@ -15,32 +19,38 @@ class GUI:
         self.enter_text_widget = None
         self.join_button = None
         self.exit_button = None
-        self.initialize_socket()
-        self.initialize_gui()
+        self.init_socket()
+        self.init_gui()
         self.listen_for_incoming_messages_in_a_thread()
 
-    def initialize_socket(self):
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # initialazing socket with TCP and IPv4
-        remote_ip = ''  # IP address
-        remote_port = 10319  # TCP port
-        self.client_socket.connect((remote_ip, remote_port))  # connect to the remote server
+    def init_socket(self):
+        # initialize the socket
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_ip = '127.0.0.1'
+        server_port = 5000
+        # connect to the server
+        self.client_socket.connect((server_ip, server_port))
 
-    def initialize_gui(self):  # GUI initializer
+
+    # intialize the GUI
+    def init_gui(self):  # GUI initializer
         self.root.title("COMPUTER NETWORKS SECRET CHAT")
         self.root.resizable(0, 0)
         self.display_chat_box()
         self.display_name_section()
         self.display_chat_entry_box()
+        self.display_emoji_buttons()
+        self.display_emoji_buttons1()
 
+    # Create a thread to receive messages  continuously
     def listen_for_incoming_messages_in_a_thread(self):
-        thread = threading.Thread(target=self.receive_message_from_server,
-                                  args=(self.client_socket,))  # Create a thread for the send and receive in same time
+        # Create a thread for sending and receiving at the same time
+        thread = threading.Thread(target=self.receive_message,
+                                  args=(self.client_socket,))  
         thread.start()
 
-    # function to recieve msg
-
-
-    def receive_message_from_server(self, so):
+    # Funciton to receive messages
+    def receive_message(self, so):
         while True:
             buffer = so.recv(256)
             if not buffer:
@@ -63,18 +73,65 @@ class GUI:
 
         so.close()
 
+
+    def display_emoji_buttons(self):
+        frame = Frame()
+
+        emoji_buttons = [
+            "😃", "😊", "👍", "👎","🧐", "👻","✋","👌","🦴"  # Add more emojis as needed
+        ]
+        emoji_x_pos = 490
+
+        for emoji in emoji_buttons:
+            button = Button(frame,text=emoji, width=7, command=lambda emoji=emoji: self.on_emoji_button_clicked(emoji))
+            button.pack(side='left', padx=(7, 0))
+            emoji_x_pos += 30
+
+        frame.pack(side='bottom')
+    def display_emoji_buttons1(self):
+        frame = Frame()
+
+        emoji_buttons1 = [
+             "🎉", "❤️", "😄", "😍","😶", "🙄","😡" ,"😑","☠️"# Add more emojis as needed
+        ]
+        emoji_x_pos = 490
+
+        for emoji in emoji_buttons1:
+            button = Button(frame,text=emoji, width=7, command=lambda emoji=emoji: self.on_emoji_button_clicked(emoji))
+            button.pack(side='left', padx=(7, 0))
+            emoji_x_pos += 30
+
+        frame.pack(side='bottom')
+
+    def on_emoji_button_clicked(self, emoji):
+        self.enter_text_widget.insert('end', emoji)
+
     def display_name_section(self):
         frame = Frame()
-        Label(frame, text='Enter your name:', font=("Helvetica", 16)).pack(side='left', padx=10)
+        Label(frame, text='Enter your name:', font=("Arial", 10)).pack(side='left', padx=55)
         self.name_widget = Entry(frame, width=50, borderwidth=2)
         self.name_widget.pack(side='left', anchor='e')
-        self.join_button = Button(frame, text="Join", width=10, font="Helvetica 10 bold", bg="#ABB2B9", command=self.on_join).pack(side='left',padx=(10, 0))
-        self.exit_button = Button(frame, text="Exit", width=10, font="Helvetica 10 bold", bg="#ABB2B9", command=self.on_close_window).pack(side='left',padx=(10, 10))
+        self.join_button = Button(frame, text="Join", width=10, font="Arial 10 bold", bg="#ABB2B9", command=self.on_join).pack(side='left',padx=(10, 0))
+        self.exit_button = Button(frame, text="Exit", width=10, font="Arial 10 bold", bg="#ABB2B9", command=self.on_close_window).pack(side='left',padx=(10, 10))
         frame.pack(side='top', anchor='nw')
 
     def display_chat_box(self):
         frame = Frame()
-        Label(frame, text='Chat Box:', font=("Serif", 12)).pack(side='top', anchor='w')
+        Label(frame, text='AGUapp:', font=("Serif", 12)).pack(side='top', anchor='w')
+         # Fetch the image from a web URL
+        image_url = "https://cdn0.iconfinder.com/data/icons/social-media-with-fill/64/telegram_colour-512.png"  # Replace with your image URL
+        response = requests.get(image_url, stream=True)
+        image = Image.open(response.raw)
+        image = image.resize((150, 150))  # Adjust the size of the image as per your needs
+
+        # Convert the image to Tkinter-compatible format
+        photo = ImageTk.PhotoImage(image)
+
+        # Create a label to display the image
+        image_label = Label(frame, image=photo)
+        image_label.image = photo  # Store a reference to the image to prevent it from being garbage collected
+        image_label.pack(side='left', padx=10)
+
         self.chat_transcript_area = Text(frame, width=60, height=10, font=("Serif", 12))
         scrollbar = Scrollbar(frame, command=self.chat_transcript_area.yview, orient=VERTICAL)
         self.chat_transcript_area.config(yscrollcommand=scrollbar.set)
@@ -85,11 +142,12 @@ class GUI:
 
     def display_chat_entry_box(self):
         frame = Frame()
-        Label(frame, text='Enter message:', font=("Serif", 12)).pack(side='top', anchor='w')
+        Label(frame, text='Enter message:', font=("Arial", 10)).pack(side='top', anchor='w')
+
         self.enter_text_widget = Text(frame, width=60, height=3, font=("Serif", 12))
         self.enter_text_widget.pack(side='left', pady=15)
         self.enter_text_widget.bind('<Return>', lambda event: self.on_send_button_clicked())
-        self.send_button = Button(frame, text="Send", width=10, font="Helvetica 10 bold", bg="#ABB2B9",command=self.on_send_button_clicked).pack(side='left', padx=(10, 0))
+        self.send_button = Button(frame, text="Send", width=10, font="Arial 10 bold", bg="#ABB2B9",command=self.on_send_button_clicked).pack(side='left', padx=(10, 0))
 
         frame.pack(side='top')
 
@@ -99,6 +157,7 @@ class GUI:
             return
         self.send_chat()
         self.clear_text()
+
     def on_join(self):
         if len(self.name_widget.get()) == 0:
             messagebox.showerror(
@@ -107,6 +166,7 @@ class GUI:
         self.name_widget.config(state='disabled')
         self.client_socket.send(("joined:" + self.name_widget.get()).encode('utf-8'))
 
+
     def on_enter_key_pressed(self, event):
         if len(self.name_widget.get()) == 0:
             messagebox.showerror("Enter your name", "Enter your name to send a message")
@@ -114,13 +174,17 @@ class GUI:
         self.send_chat()
         self.clear_text()
 
+
     def clear_text(self):
         self.enter_text_widget.delete(1.0, 'end')
 
     def send_chat(self):
         senders_name = self.name_widget.get().strip() + ": "
         data = self.enter_text_widget.get(1.0, 'end').strip()
-        message = (senders_name + data).encode('utf-8')
+
+        emoji = " 👤 "
+
+        message = (emoji +(senders_name)+ "\n" + data).encode('utf-8')
         self.chat_transcript_area.insert('end', message.decode('utf-8') + '\n')
         self.chat_transcript_area.yview(END)
         self.client_socket.send(message)
